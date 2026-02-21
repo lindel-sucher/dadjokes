55fb67ecbd43856f744a0de65ed744cfb39f6dcc
Abby Sucher <suchera@vcu.edu>
2026-02-21 12:18:38 -0500

    replace inner html with text content

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 const jokeEl = document.getElementById('joke');
 const jokeBtn = document.getElementById('jokeBtn');
 
-function generateJoke() {
-   const config = {
-     headers: {
-       Accept: 'application/json',
-     },
-   };
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  };
 
-   fetch('https://icanhazdadjoke.com', config)
-     .then((res) => res.json())
-     .then((data) => {
-       jokeEl.innerHTML = data.joke;
-     });
- }
+  const res = await fetch('https://icanhazdadjoke.com', config);
+  const data = await res.json();
+
+  jokeEl.textContent = data.joke;
+}
 
 generateJoke();
+
+//event listener
+jokeBtn.addEventListener('click', generateJoke);
